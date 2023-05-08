1872dc7a7aac9c02c274e4e4bea6c2e4d636dac8
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-05-09 01:23:46 +0300

    add create new account

--- src/Screens/LoginScreen.js
+++ src/Screens/LoginScreen.js
@@ -1,15 +1,63 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, TextInput, Image, Text, TouchableOpacity} from 'react-native';
-import gmailLogo from '../../assets/icon_gmail.svg';
+import {StyleSheet, View, TextInput, Image, Text, TouchableOpacity, Alert} from 'react-native';
+import {signInWithEmailAndPassword, getRedirectResult, GoogleAuthProvider} from "firebase/auth";
+import {auth} from "../firebase/firebase";
+import TextDivider from "./components/TextDivider";
+
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const handleLogin = () => {
+        signInWithEmailAndPassword(auth, email, password)
+            .then((user) => {
+                console.log(user)
+                Alert.alert(user.user.email)
+            })
+            .catch((error) => {
+                console.log(error.message)
+            });
+    };
+
+    const onPressForgotPassword = () => {
+        Alert.alert('Forget Password')
+    };
+
+    const onPressSignUp = () => {
+        Alert.alert('navigation.navigate("Register")')
+        // navigation.navigate("Register");
+    };
+
+    const handleLoginWithGoogle = () => {
+        // Alert.alert('Gmail login')
+        // auth.signInWithPopup(provider)
+        //     .then((result) => {
+        //         console.log('result', result)
+        //     })
+        //     .catch((error) => {
+        //         console.log('error', error)
+        //     })
+
+        // signInWithRedirect(auth, provider)
+        //     .then((result) => {
+        //         const credential = GoogleAuthProvider.credentialFromResult(result);
+        //         const token = credential.accessToken;
+        //         const user = result.user;
+        //     }).catch((error) => {
+        //     const errorCode = error.code;
+        //     const errorMessage = error.message;
+        //     const email = error.customData.email;
+        //     const credential = GoogleAuthProvider.credentialFromError(error);
+        // });
+    };
+
     return (
         <View style={[styles.container, StyleSheet.absoluteFill]}>
             <Image source={require('../../assets/logo.png')} style={styles.logo}/>
+
             <Text style={styles.title}>Welcome Back!</Text>
+
             <TextInput
                 style={[styles.input, {
                     marginBottom: 20,
@@ -19,6 +67,7 @@ const LoginScreen = () => {
                 onChangeText={(text) => setEmail(text)}
                 value={email}
             />
+
             <TextInput
                 style={styles.input}
                 placeholder="Password"
@@ -27,14 +76,43 @@ const LoginScreen = () => {
                 onChangeText={(text) => setPassword(text)}
                 value={password}
             />
+
             <TouchableOpacity style={[styles.button, {
                 marginTop: 20,
-            }]}>
+            }]} onPress={handleLogin}>
                 <Text style={styles.buttonText}>Login</Text>
             </TouchableOpacity>
-            <Text style={styles.forgotPassword}>Forgot Password?</Text>
 
-            <Image source={gmailLogo} style={styles.logo} />
+            <TouchableOpacity onPress={onPressForgotPassword}>
+                <Text style={styles.hintText}>Forgot Password?</Text>
+            </TouchableOpacity>
+
+            <TextDivider>
+                <Text style={styles.hintText}>OR</Text>
+            </TextDivider>
+
+            <TouchableOpacity onPress={handleLoginWithGoogle}>
+                <Image source={require('../../assets/icon_gmail.png')} style={{
+                    width: 44,
+                    marginTop: 20,
+                    alignSelf: 'center'
+                }}/>
+                <Text style={[styles.buttonText, {marginLeft: 8}]}>Sign in with Gmail</Text>
+            </TouchableOpacity>
+
+            <View style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+            }}>
+                <Text style={[styles.hintText]}>
+                    Don't have an account?
+                </Text>
+                <TouchableOpacity onPress={onPressSignUp}>
+                    <Text style={[styles.textClick, {marginStart: 5}]}>
+                        Create a new account
+                    </Text>
+                </TouchableOpacity>
+            </View>
 
         </View>
     );
@@ -70,10 +148,9 @@ const styles = StyleSheet.create({
         fontFamily: 'Roboto',
         color: 'rgba(0,0,0,0.6)',
     },
-    forgotPassword: {
+    hintText: {
         color: 'rgba(18,18,18,0.6)',
-        fontStyle: 'normal',
-        fontWeight: 500,
+        fontWeight: 600,
         fontSize: 12,
         marginTop: 16,
         alignSelf: 'center',
@@ -96,6 +173,17 @@ const styles = StyleSheet.create({
         color: '#FFFFFF',
         fontSize: 16,
         fontWeight: '400',
+    },
+    textClick: {
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: '800',
+        marginTop: 16,
+        fontSize: 12,
+        lineHeight: 18,
+        color: 'rgba(0, 0, 0, 0.6)',
+        flex: 0,
+        flexGrow: 0
     },
 });
 
