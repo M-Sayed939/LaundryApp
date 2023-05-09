9fbe5329bd4db9bbddf7316e0b875f37da3ef2be
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-05-09 13:16:49 +0300

    add validation

--- src/Screens/LoginScreen.js
+++ src/Screens/LoginScreen.js
@@ -1,26 +1,50 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, Image, Text, TouchableOpacity, Alert} from 'react-native';
+import {StyleSheet, View, Image, Text, TouchableOpacity, Alert, ScrollView} from 'react-native';
 import {
     signInWithEmailAndPassword,
     sendPasswordResetEmail,
     GoogleAuthProvider,
     signInWithPopup
 } from "firebase/auth";
-import {auth} from "../firebase/firebase";
+import {auth, db} from "../firebase/firebase";
 import TextDivider from "./components/TextDivider";
 import CustomInputFiled from "./components/CustomInputField";
 import CustomButton from "./components/CustomButton";
-
+import {doc, setDoc} from "firebase/firestore";
 
 const LoginScreen = ({navigation}) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const provider = new GoogleAuthProvider();
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
 
     const handleLogin = () => {
+        if (!emailRegex.test(email)) {
+            Alert.alert('Invalid email');
+            return;
+        }
+
+        if (!passwordRegex.test(password)) {
+            Alert.alert('Invalid password');
+            return;
+        }
+
         signInWithEmailAndPassword(auth, email, password)
             .then((user) => {
-                console.log(user)
+                const email = user.user.email;
+                const phone = user.user.phoneNumber;
+                const userRef = doc(db, 'users', user.user.idToken);
+
+                setDoc(userRef, {
+                    email: email,
+                    phone: phone
+                }).then(r => {
+                    console.log('done')
+                }).catch((err) => {
+                    console.log(err.message)
+                })
+
                 navigation.navigate('HomeScreen');
             })
             .catch((error) => {
@@ -28,13 +52,13 @@ const LoginScreen = ({navigation}) => {
             });
     };
 
-    const handleResetPassword = (email) => {
+    const handleResetPassword = () => {
         sendPasswordResetEmail(auth, email)
             .then(() => {
                 Alert.alert('Password reset email sent successfully')
             })
             .catch((error) => {
-                console.log('Password reset email could not be sent:', error);
+                Alert.alert(`Password reset email could not be sent`)
             });
     };
 
@@ -49,6 +73,16 @@ const LoginScreen = ({navigation}) => {
                 const token = credential.accessToken;
                 const user = result.user;
                 // handle success
+                const userRef = doc(db, 'users', token);
+
+                setDoc(userRef, {
+                    email: user.email,
+                    phone: user.phoneNumber
+                }).then(r => {
+                    console.log('done')
+                }).catch((err) => {
+                    console.log(err.message)
+                })
                 navigation.navigate('HomeScreen');
             })
             .catch((error) => {
@@ -62,59 +96,63 @@ const LoginScreen = ({navigation}) => {
     };
 
     return (
-        <View style={[styles.container, StyleSheet.absoluteFill]}>
-            <Image source={require('../../assets/logo.png')} style={styles.logo}/>
+        <ScrollView style={[StyleSheet.absoluteFill]}>
+            <View style={styles.container}>
 
-            <Text style={styles.title}>Welcome Back!</Text>
+                <Image source={require('../../assets/logo.png')} style={styles.logo}/>
 
-            <CustomInputFiled
-                style={[styles.input, {marginBottom: 20,}]}
-                placeHolder="Email"
-                value={email}
-                setValue={(text) => setEmail(text)}
-                isPassword={false}/>
+                <Text style={styles.title}>Welcome Back!</Text>
 
-            <CustomInputFiled
-                style={[styles.input, {marginBottom: 20,}]}
-                placeHolder="Password"
-                value={password}
-                setValue={(text) => setPassword(text)}
-                isPassword={true}/>
+                <CustomInputFiled
+                    style={[styles.input, {marginBottom: 20,}]}
+                    placeHolder="Email"
+                    value={email}
+                    setValue={(text) => setEmail(text)}
+                    isPassword={false}/>
+
+                <CustomInputFiled
+                    style={[styles.input, {marginBottom: 20,}]}
+                    placeHolder="Password"
+                    value={password}
+                    setValue={(text) => setPassword(text)}
+                    isPassword={true}/>
 
 
-            <CustomButton text="Login" handleClick={handleLogin}/>
+                <CustomButton text="Login" handleClick={handleLogin}/>
 
-            <TouchableOpacity onPress={handleResetPassword}>
-                <Text style={styles.hintText}>Forgot Password?</Text>
-            </TouchableOpacity>
-
-            <TextDivider>
-                <Text style={styles.hintText}>OR</Text>
-            </TextDivider>
-
-            <TouchableOpacity onPress={handleLoginWithGoogle}>
-                <Image source={require('../../assets/icon_gmail.png')} style={{
-                    width: 44,
-                    marginTop: 20,
-                    alignSelf: 'center'
-                }}/>
-            </TouchableOpacity>
-
-            <View style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-            }}>
-                <Text style={[styles.hintText]}>
-                    Don't have an account?
-                </Text>
-                <TouchableOpacity onPress={onPressSignUp}>
-                    <Text style={[styles.textClick, {marginStart: 5}]}>
-                        Create a new account
-                    </Text>
+                <TouchableOpacity onPress={handleResetPassword}>
+                    <Text style={styles.hintText}>Forgot Password?</Text>
                 </TouchableOpacity>
-            </View>
 
-        </View>
+                <TextDivider>
+                    <Text style={styles.hintText}>OR</Text>
+                </TextDivider>
+
+                <TouchableOpacity onPress={handleLoginWithGoogle}>
+                    <Image source={require('../../assets/icon_gmail.png')} style={{
+                        width: 44,
+                        marginTop: 20,
+                        alignSelf: 'center'
+                    }}/>
+                </TouchableOpacity>
+
+                <View style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    marginBottom: 20
+                }}>
+                    <Text style={[styles.hintText]}>
+                        Don't have an account?
+                    </Text>
+                    <TouchableOpacity onPress={onPressSignUp}>
+                        <Text style={[styles.textClick, {marginStart: 5}]}>
+                            Create a new account
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+
+            </View>
+        </ScrollView>
     );
 };
 
@@ -128,7 +166,7 @@ const styles = StyleSheet.create({
     logo: {
         width: 150,
         height: 150,
-        marginTop: 10,
+        marginTop: 35,
         marginBottom: 20,
     },
     title: {
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,7 @@
 import {initializeApp} from "firebase/app";
-import {getAuth} from "firebase/auth";
+import {getAuth, GoogleAuthProvider} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyArh8ri99P7IfzgC43s0rqq2UOJmg1iMuw",
@@ -12,3 +14,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const provider = new GoogleAuthProvider(app);
