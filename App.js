5cb841aa8f2cbeee4d5bfb2dc6d5ccbd8c0a3f0e
AlaaKhaled101 <alaakhaled9191@gmail.com>
2023-05-09 05:36:10 +0200

    Card Screen

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CartScreen from "./src/Screens/CartScreen";
+import PickUpScreen from "./src/Screens/PickUpScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CartScreen/>
+      <PickUpScreen/>
       <StatusBar style="auto" />
     </View>
   );
