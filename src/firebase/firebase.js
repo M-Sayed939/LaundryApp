22931938189f974f6c78ce419ed01cc5e11afa4c
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-05-09 04:21:04 +0300

    config clean

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from "firebase/auth";
+import {initializeApp} from "firebase/app";
+import {getAuth} from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyArh8ri99P7IfzgC43s0rqq2UOJmg1iMuw",
